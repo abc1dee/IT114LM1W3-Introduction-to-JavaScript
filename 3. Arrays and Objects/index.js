9d683e1c4e1bcaca5a3aa938582f23c7594e5eba
Deo <154957807+abc1dee@users.noreply.github.com>
2024-01-13 09:59:51 +0000

    draft of number 3

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -14,9 +14,15 @@ let numbers = [
 
 // Todo 3.1 Get the first, 5th, and last items in the numbers array.
 // Your code here
+var firstItem = numbers[0];
+var fifthItem = numbers[4];
+var lastItem = numbers[numbers.length - 1];
 
 // Todo 3.2 calculate the min, max, and the average of the numbers array
 // Your code here
+var minNum = Math.min(...numbers);
+var maxNum = Math.max(...numbers);
+var averageNum = numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
 
 // Checkpoint 3.1 How would you compare lists in python vs arrays in JavaScript
 // Answer: 
@@ -24,11 +30,26 @@ let numbers = [
 // Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
 // Your code here
 
+let IT114L = {
+    courseCode: "IT114L",
+    name: "Introduction to Programming",
+    units: 3,
+    numberOfStudents: 30
+};
+
 // Todo 3.4 Add professor name as one of the fields of the object. Display the value of professor name.
 // Your code here
+IT114L.professorName = "Sir Job Lipat";
+console.log(IT114L.professorName);
+console.log(IT114L);
 
 // Todo 3.5 Declare and array of objects with information about the courses you are taking this term
 // Your code here
+let courses = [
+    { courseCode: "IT114L", units: 3 },
+    { courseCode: "MATH101", units: 4 },
+    { courseCode: "ENG202", units: 3 }
+];
 
 // Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
 // Your code here
